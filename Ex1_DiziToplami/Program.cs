1ba159569e1fde7dc99734deb91c544dedf1e83a
Hilal OĞUZHAN <55708261+hilaloguzhan1@users.noreply.github.com>
2022-05-17 15:10:19 +0300

    Ex1_DiziToplami
    Dışardan girilen n boyutlu bir dizinin toplamını veren uygulama

---
+++ Ex1_DiziToplami/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ex1_DiziToplami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir dizi boyutu giriniz=");
+            int [] sayiDizisi=new int[Convert.ToInt32(Console.ReadLine())];
+            int Toplam=0;
+
+            for (int i = 0; i <sayiDizisi.Length; i++)
+            {
+                Console.WriteLine(i+1 +". SAYIYI GİRİNİZ=");
+                int [] sayi = new int[sayiDizisi.Length];
+                sayi[i]=int.Parse(Console.ReadLine());
+                Toplam+=sayi[i];
+            
+
+            }
+           Console.WriteLine("Dizi Toplam= "+Toplam);
+            
+        }  
+          
+    }
+}
